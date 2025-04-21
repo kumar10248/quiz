4d9d3f1225c11e89394e70ca8ca73a8be8007878
kumar10248 <kumardevashish000@gmail.com>
2025-04-21 23:58:21 +0530

    fix: Update social media icons section and adjust container padding for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import Link from 'next/link';
-import { FaBook, FaClock, FaGraduationCap } from 'react-icons/fa';
+import { FaBook, FaClock, FaGraduationCap, FaInstagram, FaGithub, FaTwitter, FaCode, FaLinkedinIn } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <div className="container mx-auto px-4 py-16">
+      <div className="container mx-auto px-4 py-8">
+        {/* Social Media Icons and Portfolio Link */}
+        <div className="flex justify-center space-x-6 mb-8">
+        <a href="https://www.devashish.top" className="text-2xl hover:text-purple-400 transition duration-300">
+            <FaCode />
+          </a>
+          <a href="https://linkedin.com/in/kumar-devashishh" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-blue-500 transition duration-300">
+            <FaLinkedinIn />
+          </a>
+          <a href="https://instagram.com/mathmaverick_man" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-amber-400 transition duration-300">
+            <FaInstagram />
+          </a>
+          <a href="https://github.com/kumar10248" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-green-400 transition duration-300">
+            <FaGithub />
+          </a>
+          <a href="https://twitter.com/kumarDe10248" target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-blue-400 transition duration-300">
+            <FaTwitter />
+          </a>
+          
+        </div>
+        
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-3xl md:text-6xl font-bold mb-6">
             <span className="text-amber-400">NPTEL</span> Question <span className="text-green-400">Practice</span>
@@ -18,7 +38,7 @@ export default function Home() {
             Start Practicing
           </Link>
           <div>
-            <a href="/W7_merged.pdf" download= "complete_IOT-Notes.pdf"
+            <a href="/W7_merged.pdf" download="complete_IOT-Notes.pdf"
             className='inline-block bg-green-500 hover:bg-green-600 text-black font-bold py-3 px-8 rounded-lg transition duration-300 text-lg mt-4'
             >
               Download Notes</a>
@@ -41,8 +61,6 @@ export default function Home() {
               <p className="text-gray-400">Monitor your scores and improvement over time</p>
             </div>
           </div>
-          
-          
         </div>
       </div>
     </div>
